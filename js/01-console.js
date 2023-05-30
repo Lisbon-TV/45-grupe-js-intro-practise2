930f99f762e8d848a2261cde11cc368c9f8dd7ad
Lisbon <lisbon@dublin.com>
2023-05-30 11:50:30 +0300

    Numbers

--- js/01-console.js
+++ js/01-console.js
@@ -1,1 +1,7 @@
+// tuscia eilute
+console.log();
+
+console.log(1);
+console.log(1, 2, 2);
+
 console.clear();
---
+++ js/02-numbers.js
@@ -0,0 +1,13 @@
+console.clear();
+
+console.log(2.13);
+console.log(-5675.3125);
+console.log(Infinity);
+console.log(NaN);
+console.log(2 + 3);
+console.log(2 + 3, 5);
+console.log(2 + 2 * 2, 8);
+console.log((2 + 2) * 8);
+
+//ignoruotif paklaida
+console.log(0.1 + 0.2);

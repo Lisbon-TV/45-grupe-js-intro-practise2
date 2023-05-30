f8ec6c02faee1b9060ad70ab34611bfacfdde267
Lisbon <lisbon@dublin.com>
2023-05-30 12:10:51 +0300

    Constant and let numbers

--- js/02-numbers.js
+++ js/02-numbers.js
@@ -11,3 +11,23 @@ console.log((2 + 2) * 8);
 
 //ignoruotif paklaida
 console.log(0.1 + 0.2);
+
+console.clear();
+
+const dishes = 20;
+console.log(dishes);
+
+const amziausSkirtumas = 5;
+console.log(amziausSkirtumas);
+
+let pinigine = 35;
+console.log(pinigine);
+console.log(pinigine);
+console.log(pinigine);
+
+pinigine = pinigine + 4;
+console.log(pinigine);
+
+pinigine = 45;
+console.log(pinigine);
+
